f65ad130d0ca9a91a98add744bc3b15743755850
vaikunthtrehan <69431166+vaikunthtrehan@users.noreply.github.com>
2021-03-21 15:54:16 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,27 @@
+var ball;
+var database,position
+var form,game,player
+var gameState=0
+var playerCount
+var allPlayers
+function setup(){
+    database=firebase.database()
+    createCanvas(500,500);
+   
+    game=new Game()
+    game.getState()
+    game.start()
+}
+
+function draw(){
+    background("white");
+   if(playerCount===4){
+  game.update (1)
+   }
+   if(gameState===1){
+ clear ()
+ game.play()
+
+   }
+}
+
